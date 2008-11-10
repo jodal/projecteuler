13ecbdcdda4ac2b8906ecf8fbf0b6a10db21c814
Stein Magnus Jodal <stein.magnus@jodal.no>
2008-11-10 17:12:15 +0100

    2.7x speedup of problem 4

--- problem004.py
+++ problem004.py
@@ -1,12 +1,11 @@
+from eulerlib import is_palindromic
+
 def palindrome():
     max = 0
     for i in range(100, 1000):
         for j in range(100, 1000):
             product = i * j
-            straight = list(str(product))
-            reverse = list(str(product))
-            reverse.reverse()
-            if straight == reverse and product > max:
+            if is_palindromic(product) and product > max:
                 max = product
     return max
 
