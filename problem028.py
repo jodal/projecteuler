cc9293955235428605ba80ef944a6025dc1b6325
Stein Magnus Jodal <stein.magnus@jodal.no>
2008-11-11 23:35:35 +0100

    Solve problem 28

---
+++ problem028.py
@@ -0,0 +1,15 @@
+def problem028(n):
+    odd_squares = [i ** 2 for i in range(n + 1) if i % 2 == 1]
+    result = 1
+    step = 2
+    i = 1
+    while i < n ** 2:
+        i += step
+        result += i
+        if i in odd_squares:
+            step += 2
+    return result
+
+if __name__ == '__main__':
+    assert problem028(5) == 101
+    print problem028(1001)
